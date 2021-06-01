880cf3ab4c12cbeb601e33cdbc8b125cec0280ca
Greg Mozer <mozer.grzegorz@yahoo.com>
2021-06-01 01:04:34 -0400

    update Dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 
-const Dropdown = ({ options, selected, onSelectedChange }) => {
+const Dropdown = ({ options, selected, onSelectedChange, label }) => {
   const [toggle, setToggle] = useState(false);
   const ref = useRef();
 
@@ -39,7 +39,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
   return (
     <div ref={ref} className="ui form">
       <div className="field">
-        <label className="label">Select a color</label>
+        <label className="label">{label}</label>
         <div
           onClick={() => setToggle(!toggle)}
           className={`ui selection dropdown ${toggle ? "visible active" : ""}`}
